fe0a67e9935186b439e57624fd5f0b4b227becbe
Geohalbert <geohalbert@gmail.com>
2017-10-19 13:41:54 -0500

    can't figure out how to switch playerTurn

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -7,14 +7,34 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
+  [' ', 'X', ' '],
+  ['O', ' ', ' '],
+  [' ', 'X', ' ']
 ];
 
-let playerTurn = 'X';
+
+
+
+  // if (playerTurn === 'X'){
+  //   playerTurn === 'O';
+  //   return playerTurn;
+  // }else if (playerTurn === 'O') {
+  //   playerTurn === 'X';
+  //   return playerTurn;
+  // }
+  // turn === turn+1;
+  //   if (turn%2 === 0) {
+  //     playerTurn === 'X';
+  //     return playerTurn;
+  //   }else if (turn%2 === 1) {
+  //     playerTurn === '0';
+  //     return playerTurn;
+  //   }
+
+
 
 function printBoard() {
+  let playerTurn, sum, total;
   console.log('   0  1  2');
   console.log('0 ' + board[0].join(' | '));
   console.log('  ---------');
@@ -22,33 +42,86 @@ function printBoard() {
   console.log('  ---------');
   console.log('2 ' + board[2].join(' | '));
 }
+// let playerTurn = 'X';
+
 
 function horizontalWin() {
-  // Your code here
-}
+  for (var r=0; r<3; r++) {
+    let line = board[r];
+    if ((line[0] === line[1] && line[1] === line[2]) && (line[0] === 'X' || line[0] === 'O')) {
+      return true;
+      }
+    }
+  };
+
 
 function verticalWin() {
-  // Your code here
-}
+  for (var c=0; c<3; c++) {
+    let line = [];
+    for (var r=0; r<3; r++){
+      line.push(board[r][c]);
+    }if ((line[0] === line[1] && line[1] === line[2]) && (line[0] === 'X' || line[0] === 'O')) {
+      return true;
+      }
+    }
+  };
 
 function diagonalWin() {
-  // Your code here
-}
+  if ((board[0][0] === 'X' || board[0][0] === 'O') && ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2]))) {
+    return true;
+  }else if ((board[0][2] === 'X' || board[0][2] === 'O') && ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0]))) {
+    return true;
+  }
+};
 
 function checkForWin() {
-  // Your code here
+  if (horizontalWin() == true) {
+    return true;
+  }else if (verticalWin() == true) {
+    return true;
+  }else if (diagonalWin() == true) {
+    return true;
+  }
+}
+function turns(){
+  let playerTurn = 'X';
+  let sum = [];
+  let total = sum.length
+  for (var r=0; r<3; r++) {
+    for (var c=0; c<3; c++){
+      // console.log('(board['+r+']['+c+']): ' +(board[r][c]));
+      if ((board[r][c]) === 'X' || (board[r][c]) === 'O') {
+        sum.push(1);
+      }
+    }
+  }
+  if (total%2 === 0) {
+    playerTurn = 'X';
+  }else if (total%2 === 1) {
+    playerTurn = 'O';
+  }console.log('playerTurn at end of turns(): '+playerTurn);
 }
 
-function ticTacToe(row, column) {
-  // Your code here
+function ticTacToe(row,column) {
+  row === parseInt(row);
+  console.log(row);
+  column === parseInt(column);
+  console.log(column);
+  console.log('board[row][column]: '+board[row][column]);
+  if (board[row][column] !== 'X' && board[row][column] !== 'O') {
+    board[row][column] == playerTurn;
+  }
 }
 
 function getPrompt() {
+  // let playerTurn;
   printBoard();
+  turns();
   console.log("It's Player " + playerTurn + "'s turn.");
   rl.question('row: ', (row) => {
     rl.question('column: ', (column) => {
-      ticTacToe(row, column);
+      ticTacToe(row,column);
+      checkForWin();
       getPrompt();
     });
   });
@@ -75,7 +148,7 @@ if (typeof describe === 'function') {
       assert.equal(verticalWin(), true);
     });
     it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+      board = [ [' ', ' ', ' '], ['X', 'X', 'X'], [' ', ' ', ' '] ];
       assert.equal(horizontalWin(), true);
     });
     it('should check for diagonal wins', () => {
